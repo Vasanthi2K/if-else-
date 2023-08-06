cbce278426977d9e128466072198c5b117c719d3
Vasanthi2K <70060435+Vasanthi2K@users.noreply.github.com>
2023-08-06 20:54:58 +0530

    Add files via upload

--- Dictionary.py
+++ Dictionary.py
@@ -44,6 +44,48 @@ thedict = {
 }
 print(type(thedict))
 '''
+'''
 #Creating a dictionary using dict() constructor
 thedict = dict(name = "Vasu", age = "23", country = "India")
 print(thedict)
+'''
+#Accessing Items of a dictionary
+'''
+1)Using Sqare brackets
+thedict = {
+    "company": "Aurobindo",
+    "Existence":False,
+    "Pincode":521001,
+    "IndustryType":["Pharma", "Finance", "Technology"]
+}
+print(thedict["Pincode"])
+
+2)sing get()
+thedict = {
+    "company": "Aurobindo",
+    "Existence":False,
+    "Pincode":521001,
+    "IndustryType":["Pharma", "Finance", "Technology"]
+}
+print(thedict.get("Existence"))
+'''
+'''
+#Get Keys
+#This method returns list of all keys in the Dictionary
+thedict = {
+    "company": "Aurobindo",
+    "Existence":False,
+    "Pincode":521001,
+    "IndustryType":["Pharma", "Finance", "Technology"]
+}
+print(thedict.keys())
+'''
+#Get Values
+#The Values() method will return list of all the values in the dictionary.
+thedict = {
+    "company": "Aurobindo",
+    "Existence":False,
+    "Pincode":521001,
+    "IndustryType":["Pharma", "Finance", "Technology"]
+}
+print(thedict.values())
